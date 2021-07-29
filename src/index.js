4d70a51968f4601285052b5ae90bb8bcf5faa3db
Hassan Zafar <hassanzafar619@example.com>
2021-07-29 20:15:59 +0500

    footer added

--- src/index.js
+++ src/index.js
@@ -1,46 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Header from './components/header'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Link, Switch , Redirect } from "react-router-dom";
+import Header from "./components/header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
 import Form from "./components/login/Form";
 import Cards from "./components/card/card";
-import {Row , Col} from "react-bootstrap";
-
-
-
+import { Row, Col } from "react-bootstrap";
+import FooterPage from "./components/footer";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
- <BrowserRouter>
- 
- <Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        {" "}
+        <Header />
+        <Cards />
+        <FooterPage/>
+      </Route>
 
+      <Route path="/login">
+        {" "}
+        <Form />{" "}
+      </Route>
 
-
-<Route  exact path="/" >  <Header/>
-
-
-
-         <Cards/> 
-
- 
-
-    </Route>
-
-<Route path="/login">   <Form/> </Route>
-
-
-<Redirect to="/" />
-
- </Switch>
-
- </BrowserRouter>,
- rootElement
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>,
+  rootElement
 );
 reportWebVitals();
-
